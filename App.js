c5c4cc10b321c8ba15cd2d4c8a8eaa83302d7461
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-12-17 17:15:59 -0300

    T8S3-77 html + css (sin js)

--- App.js
+++ App.js
@@ -14,6 +14,10 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join( __dirname , 'public' , 'login.html'));
 });
 
+app.get('/cargarSaldo', (req, res) => {
+  res.sendFile(path.join( __dirname , 'public/cargarSaldo' , 'cargarSaldo.html'));
+});
+
 app.post("/loginback", async (req, res) => {
 try{
   const data = req.body;
